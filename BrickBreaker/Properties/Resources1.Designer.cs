6d2af49f51a7427072a1e24ee8ac255425db0d99
benjlebe741 <benjlebe741@ed.amdsb.ca>
2024-05-09 10:02:36 -0400

    yes

--- BrickBreaker/Properties/Resources1.Designer.cs
+++ BrickBreaker/Properties/Resources1.Designer.cs
@@ -338,6 +338,16 @@ namespace BrickBreaker.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap fireball {
+            get {
+                object obj = ResourceManager.GetObject("fireball", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Friend2 {
             get {
                 object obj = ResourceManager.GetObject("Friend2", resourceCulture);
